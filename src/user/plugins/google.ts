89cc2b8ced223eaa64274c1e061f891279d00bf6
nguyenlongdang0412 <nguyenlongdang04121999@gmail.com>
2024-09-11 22:09:10 +0700

    google callback

--- src/user/plugins/google.ts
+++ src/user/plugins/google.ts
@@ -2,6 +2,23 @@
 import { Elysia } from 'elysia'
 import { oauth2 } from 'elysia-oauth2'
 
+// ** Prisma Imports
+import prismaClient from '@src/database/prisma'
+
+const fetchGoogleUserInfo = async (accessToken: string) => {
+    const response = await fetch('https://people.googleapis.com/v1/people/me?personFields=phoneNumbers,photos,emailAddresses,names', {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    })
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch user info from Google API')
+    }
+
+    return response.json()
+}
+
 const googleUserPlugin = (app: Elysia) =>
     app.use(
         oauth2({
@@ -11,22 +28,35 @@ const googleUserPlugin = (app: Elysia) =>
                 Bun.env.GOOGLE_CALLBACK_URL as string
             ]
         })
-            .get('/google', async ({ oauth2, redirect }) => {
+            .get('/google', async ({ oauth2 }) => {
                 const authorizationUrl = await oauth2.createURL('Google', {
                     scopes: [
                         'https://www.googleapis.com/auth/userinfo.profile',
-                        'https://www.googleapis.com/auth/userinfo.email'
+                        'https://www.googleapis.com/auth/userinfo.email',
+                        'https://www.googleapis.com/auth/user.phonenumbers.read'
                     ]
                 })
 
                 authorizationUrl.searchParams.set('access_type', 'offline')
                 authorizationUrl.searchParams.set('prompt', 'consent')
 
-                return redirect(authorizationUrl.toString())
+                return authorizationUrl.toString()
             })
             .get('/google/callback', async ({ oauth2, cookie: { redirectUrl, userRefreshToken }, error, redirect }) => {
                 try {
                     const token = await oauth2.authorize('Google')
+
+                    const userInfo = await fetchGoogleUserInfo(token.accessToken)
+
+                    await prismaClient.users.create({
+                        data: {
+                            name: userInfo.names[0].displayName,
+                            email: userInfo.emailAddresses[0].value,
+                            email_verified: userInfo.emailAddresses[0].metadata.verified,
+                            image_uri: userInfo.photos[0].url
+
+                        }
+                    })
 
                     if (token.refreshToken) {
                         userRefreshToken.set({
