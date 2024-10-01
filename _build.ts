63d684f550d7e2253041f2cc7128ffb158825b53
nguyenlongdang0412 <130326222+nguyenlongdang0412@users.noreply.github.com>
2024-10-01 16:52:02 +0700

    build

---
+++ _build.ts
@@ -0,0 +1,16 @@
+import { $ } from 'bun'
+import dts from 'bun-plugin-dts'
+import packageJson from './package.json'
+
+await $`rm -rf dist`
+
+const result = await Bun.build({
+    entrypoints: ['src/index.ts'],
+    external: Object.keys(packageJson.dependencies),
+    outdir: 'dist',
+    target: 'node',
+    minify: true,
+    plugins: [dts({ output: { noBanner: !0 } })]
+})
+
+console.log(result)
