06d7aeb5565bf67e0333e9b73f44681982038161
nguyenlongdang0412 <nguyenlongdang04121999@gmail.com>
2024-11-30 21:28:55 +0700

    seed set redis flushall

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -7,14 +7,19 @@ import { SeedProductAttributeClass } from './product-attribute/product-attribute
 import { SeedProductBrandClass } from './product-brand/product-brand.class'
 import { SeedProductCategoryClass } from './product-category/product-category.class'
 
+// ** Plugins Imports
+import { redisPlugin } from '@src/plugins/redis'
+
 export const seed = new Elysia({ prefix: '/seed' })
+    .use(redisPlugin)
     .decorate({
         SeedAuthClass: new SeedAuthClass(),
         SeedProductCategoryClass: new SeedProductCategoryClass(),
         SeedProductBrandClass: new SeedProductBrandClass(),
         SeedProductAttributeClass: new SeedProductAttributeClass()
     })
-    .get('/', async ({ SeedAuthClass, SeedProductCategoryClass, SeedProductBrandClass, SeedProductAttributeClass }) => {
+    .get('/', async ({ redis, SeedAuthClass, SeedProductCategoryClass, SeedProductBrandClass, SeedProductAttributeClass }) => {
+        await redis.forgetAll()
         await SeedAuthClass.authSeedCreate()
         await SeedProductCategoryClass.productCategorySeedCreate()
         await SeedProductBrandClass.productBrandSeedCreate()
