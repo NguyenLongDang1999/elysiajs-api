ecafe47887d9017f0b4042e6f97b6dffe5ac03f8
nguyenlongdang0412 <nguyenlongdang04121999@gmail.com>
2024-12-01 14:31:58 +0700

    Prisma update cuid2 support

--- src/database/prisma/index.ts
+++ src/database/prisma/index.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 
-import cuid2Extension from './cuid2'
-
-const prismaClient = new PrismaClient().$extends(cuid2Extension)
+const prismaClient = new PrismaClient()
 
 export default prismaClient
